3085d894482c9b4d26c1518dc980f35004410839
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-15 18:43:01 +0300

    tracker 0.0.0

---
+++ src/tests/index.ts
@@ -0,0 +1,7 @@
+require('source-map-support').install();
+
+import tracker from './tracker';
+
+describe('AncientSouls/Tracker:', () => {
+  tracker();
+});
---
+++ src/tests/tracker.ts
@@ -0,0 +1,5 @@
+import { assert } from 'chai';
+import * as _ from 'lodash';
+
+export default function () {
+}
