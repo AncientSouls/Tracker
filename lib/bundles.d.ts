8366e22b694fac789026a6c54241095d69606c29
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-23 20:37:59 +0300

    0.0.3

---
+++ lib/bundles.d.ts
@@ -0,0 +1,7 @@
+import { TAsketicTracker, IAsketicTracker, IAsketicTrackerEventsList } from '../lib/asketic-tracker';
+import { IBundle } from 'ancient-cursor/lib/bundle';
+interface ITrackerToBundlesCallback<IEventsList> {
+    <IE extends keyof IEventsList>(bundles: IBundle[], eventName: IE, eventData: IEventsList[IE]): void;
+}
+declare const trackerToBundles: (tracker: IAsketicTracker<IAsketicTrackerEventsList>, callback: ITrackerToBundlesCallback<IAsketicTrackerEventsList>) => void;
+export { trackerToBundles, TAsketicTracker, ITrackerToBundlesCallback };
