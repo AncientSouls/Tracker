0fc82adca24baa2c80923fe251d9c1a71577325f
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-28 21:24:52 +0300

    0.0.13
    Generic item

--- lib/tracking.d.ts
+++ lib/tracking.d.ts
@@ -1,13 +1,13 @@
 import { TClass, IInstance } from 'ancient-mixins/lib/mixins';
 import { INode, INodeEventsList } from 'ancient-mixins/lib/node';
 import { TTracker, ITrackerItem, ITrackerStart } from './tracker';
-declare type TTracking = ITracking<ITrackingItem, ITrackingEventsList>;
+declare type TTracking = ITracking<ITrackingItem, ITrackingEventsList<ITrackingItem>>;
 interface ITrackingItem {
     query: any;
     tracker: TTracker;
     tracking: TTracking;
 }
-interface ITracking<ITE extends ITrackingItem, IEventsList extends ITrackingEventsList> extends INode<IEventsList> {
+interface ITracking<ITE extends ITrackingItem, IEventsList extends ITrackingEventsList<ITrackingItem>> extends INode<IEventsList> {
     start(): Promise<void>;
     stop(): Promise<void>;
     fetch(query: any): Promise<any[]>;
@@ -18,12 +18,12 @@ interface ITracking<ITE extends ITrackingItem, IEventsList extends ITrackingEven
     track(query: any): ITrackerStart;
     override(tracking: ITE): Promise<void>;
 }
-interface ITrackingEventsList extends INodeEventsList {
-    tracked: ITrackingItem;
-    untracked: ITrackingItem;
-    overrided: ITrackingItem;
-    started: ITrackingItem;
-    stopped: ITrackingItem;
+interface ITrackingEventsList<ITE extends ITrackingItem> extends INodeEventsList {
+    tracked: ITE;
+    untracked: ITE;
+    overrided: ITE;
+    started: ITE;
+    stopped: ITE;
 }
 declare function mixin<T extends TClass<IInstance>>(superClass: T): any;
 declare const MixedTracking: TClass<TTracking>;
